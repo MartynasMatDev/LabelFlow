89194c3913c6609f0e288e11e9a5c1cb813a760a
MartynasMatDev <57892134+MartynasMatDev@users.noreply.github.com>
2026-03-01 23:41:04 +0200

    Added Project model to Django admin functionality

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display  = ('name', 'created_by', 'annotation_type', 'member_count', 'image_count', 'created_at')
+    list_filter   = ('annotation_type',)
+    search_fields = ('name', 'created_by__username')
+    #inlines       = [ProjectMemberInline]
+
+
