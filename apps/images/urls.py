2ce682c2558e4dacde20a75ea8260b351c12864a
Deimante <miseikyte3000@gmail.com>
2026-03-03 22:04:17 +0200

    added ability to upload an image

---
+++ apps/images/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',                              views.image_list,   name='image_list'),
+    path('upload/',                       views.image_upload, name='image_upload'),
+    path('project/<int:project_id>/',     views.image_list,   name='project_images'),
+    path('<int:image_id>/delete/',        views.image_delete, name='image_delete'),
+]
