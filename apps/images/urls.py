fdb5112b35115f26c17b16f8659bc3073a067fd0
Deimante <miseikyte3000@gmail.com>
2026-03-03 23:52:52 +0200

    added the ability to view a list of images and see a larger version when clicked

--- apps/images/urls.py
+++ apps/images/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('upload/',                       views.image_upload, name='image_upload'),
     path('project/<int:project_id>/',     views.image_list,   name='project_images'),
     path('<int:image_id>/delete/',        views.image_delete, name='image_delete'),
+    path('<int:pk>/', views.image_detail, name='image_detail'),
+
 ]
