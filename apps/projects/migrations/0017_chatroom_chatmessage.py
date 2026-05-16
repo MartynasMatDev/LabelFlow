b8ea6f410a7932ca07e8f14bf20fa5f7cbd6aae1
MartynasMatDev <57892134+MartynasMatDev@users.noreply.github.com>
2026-05-16 18:05:30 +0300

    Fixed migration duplicate file issue

--- apps/projects/migrations/0017_chatroom_chatmessage.py
+++ apps/projects/migrations/0017_chatroom_chatmessage.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0015_alter_activitylog_action_notification'),
+        ('projects', '0016_task_taskcomment'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
